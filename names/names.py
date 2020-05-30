583f32045d117e29858540e22d4581ad71ebd18c
jackskim <jackskim29@gmail.com>
2020-05-29 20:04:08 -0700

    Add BST solution for names.py

--- names/names.py
+++ names/names.py
@@ -1,3 +1,56 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif (target  < self.value) and (self.left is not None):
+            return self.left.contains(target)
+        elif (target > self.value) and (self.right is not None):
+            return self.right.contains(target)
+        else:
+            return False
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+        # current_node = self
+        # while current_node.right is not None:
+        #     current_node = current_node.right
+        # return current_node.value
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+
+        if self.left is not None:
+            self.left.for_each(fn)
+        if self.right is not None:
+            self.right.for_each(fn)
+
+
 import time
 
 start_time = time.time()
@@ -12,15 +65,35 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+## Runtime of nested loops is O(n**2)
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#for name_1 in names_1:
+#    for name_2 in names_2:
+#        if name_1 == name_2:
+#            duplicates.append(name_1)
+
+def comparison(name):
+    if NameBST2.contains(name):
+        duplicates.append(name)
+
+NameBST1 = BSTNode(names_1[0])
+for name_1 in names_1[1:]:
+    NameBST1.insert(name_1)
+
+NameBST2 = BSTNode(names_2[0])
+for name_2 in names_2[1:]:
+    NameBST2.insert(name_2)
+
+NameBST1.for_each(comparison)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
