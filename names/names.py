d6dc12aad9b787f3ee6fef6b114ba2e2fbd28b2e
jackskim <jackskim29@gmail.com>
2020-05-29 20:05:24 -0700

    Remove extra comments

--- names/names.py
+++ names/names.py
@@ -4,7 +4,6 @@ class BSTNode:
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
     def insert(self, value):
         if value < self.value:
             if self.left is None:
@@ -17,8 +16,6 @@ class BSTNode:
             else:
                 self.right.insert(value)
 
-    # Return True if the tree contains the value
-    # False if it does not
     def contains(self, target):
         if target == self.value:
             return True
@@ -29,19 +26,12 @@ class BSTNode:
         else:
             return False
 
-    # Return the maximum value found in the tree
     def get_max(self):
         if self.right is None:
             return self.value
         else:
             return self.right.get_max()
 
-        # current_node = self
-        # while current_node.right is not None:
-        #     current_node = current_node.right
-        # return current_node.value
-
-    # Call the function `fn` on the value of each node
     def for_each(self, fn):
         fn(self.value)
 
